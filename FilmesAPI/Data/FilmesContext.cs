c055cacf22874c6c087ed2434e9942f3ba439203
Pedro Marinho <pmaf@ic.ufal.br>
2022-10-03 00:23:42 -0300

    Initial MySQL configuration (DbContext).

---
+++ FilmesAPI/Data/FilmesContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmesContext : DbContext
+    {
+        public FilmesContext(DbContextOptions<FilmesContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,8 +1,19 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Getting the MySQL connection string from appsettings.json.
+string mySQLConnectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Connecting with MySQL database.
+builder.Services.AddDbContext<FilmesContext>(opts => opts.UseMySQL(mySQLConnectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
